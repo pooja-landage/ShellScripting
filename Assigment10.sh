28cf2fb328411122d32aefe4827f9b47e3681f46
Pooja <poojalandage22@gmail.com>
2021-10-30 23:09:38 +0530

    assigment 10

---
+++ Assigment10.sh
@@ -0,0 +1,13 @@
+echo "enter a number "
+read n
+case $n in
+	1)echo "sunday" ;;
+	2)echo "Monday" ;;
+	3)echo "Tuesday";;
+	4)echo "wednseday";;
+	5)echo "thursady";;
+	6)echo "Friday" ;;
+	7)echo "Saturday" ;;
+	*)echo "enter value between 1 to 7" ;;
+esac
+
